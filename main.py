7290a40a45c1dca8c577b6286d4a83be2f6c4ae7
Park Junha <qkrwnsgk1624@naver.com>
2022-12-18 07:26:16 +0900

    feat : add index page, music

--- main.py
+++ main.py
@@ -2,22 +2,32 @@ import sys
 
 from PyQt6.QtWidgets import *
 from PyQt6.QtGui import *
+from PyQt6.QtCore import QUrl
+from PyQt6.QtMultimedia import QAudioOutput, QMediaPlayer
 from PyQt6 import uic, QtWidgets
-from sub import Index
+from widgets.index import Index
 
 import resource_rc as resource_rc
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.stackedWidget:QStackedWidget
         uic.loadUi("ui/form.ui", self)
+        self.stackedWidget:QStackedWidget
         self.setFixedSize(1270, 720)
+        self.player = QMediaPlayer()
+        self.audio_output = QAudioOutput()
+        self.playAudioFile()
 
     def moveIndexWindow(self):
         widget.setCurrentIndex(widget.currentIndex()+1)
 
-
+    def playAudioFile(self):
+        file_path = "music/승리를위하여.mp3"
+        self.player.setAudioOutput(self.audio_output)
+        self.player.setSource(QUrl.fromLocalFile(file_path))
+        self.audio_output.setVolume(50)
+        self.player.play()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
