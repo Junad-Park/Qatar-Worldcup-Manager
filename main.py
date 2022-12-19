80cecafb8466eaf80ab6ca2f389a1a5e718c7b41
Park Junha <qkrwnsgk1624@naver.com>
2022-12-20 00:38:28 +0900

    feat : add change page
    complete basic ui

--- main.py
+++ main.py
@@ -2,36 +2,46 @@ import sys
 
 from PyQt6.QtWidgets import *
 from PyQt6.QtGui import *
-
 from PyQt6 import uic, QtWidgets
-from widgets.index import Index
+
+from utils.audio import Audio
 
 import resource_rc as resource_rc
+
+ui = ["ui/form.ui", "ui/index.ui", "ui/highlight.ui", "ui/schedule.ui", "ui/news.ui",]
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
-        uic.loadUi("ui/form.ui", self)
+        uic.loadUi(ui[0], self)
         self.stackedWidget:QStackedWidget
         self.setFixedSize(1270, 720)
+        self.audio = Audio()
 
-    def moveIndexWindow(self):
-        widget.setCurrentIndex(widget.currentIndex()+1)
+    def movePage(self):
+        target = self.sender()
+        target_num = int(target.objectName().split('_')[1])
+        uic.loadUi(ui[target_num], self)
+
+    def muteAudio(self):
+        self.audio.muteAudio()
+
+    def nextAudio(self):
+        self.audio.nextAudio()
+    
+    def previousAudio(self):
+        self.audio.previousAudio()
+    
+    def playAudioFile(self):
+        self.audio.playAudioFile()
 
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
-    # 화면 전환용 Widget 설정
-    widget = QtWidgets.QStackedWidget()
-
     # 레이아웃 인스턴스 생성
     mainwindow = MainWindow()
-    indexWindow = Index()
 
-    widget.addWidget(mainwindow)
-    widget.addWidget(indexWindow)
-
-    widget.show()
+    mainwindow.show()
     sys.exit(app.exec())
